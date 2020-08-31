aa089e7238534318156d4694297fb3b5cd5253be
Grant Miller <grant@grant-miller.com>
2020-08-31 11:21:15 -0400

    added example program in if __name__ == '__main__' and README.rst

--- oauth/__init__.py
+++ oauth/__init__.py
@@ -411,3 +411,46 @@ class AuthManager:
     def print(self, *a, **k):
         if self._debug:
             print(*a, **k)
+
+
+if __name__ == '__main__':
+    # from oauth_tools import AuthManager
+    import webbrowser
+    import creds
+    import os
+
+    if not os.path.exists('users.json'):
+        open('users.json', mode='wt').write(json.dumps({}))
+
+    authManager = AuthManager(
+        microsoftClientID=creds.clientID,
+        microsoftTenantID=creds.tenantID,
+    )
+
+    authManager.SaveIncompleteOACallback = lambda ID, data: open('incomplete.json', mode='wt').write(
+        json.dumps({ID: data}, indent=2))
+    authManager.GetIncompleteOACallback = lambda ID: json.loads(open('incomplete.json', mode='rt').read()).get(ID, None)
+
+    authManager.SaveToDBCallback = lambda ID, data: open('users.json', mode='wt').write(
+        json.dumps({ID: data}, indent=2))
+    authManager.GetFromDBCallback = lambda ID: json.loads(open('users.json', mode='rt').read()).get(ID, None)
+
+    MY_ID = 'Grant'
+
+    user = authManager.GetUserByID(MY_ID)
+    if user is None:
+        # the user has not authenticated before
+        d = authManager.CreateNewUser(MY_ID)
+        webbrowser.open(d['verification_uri'])
+        print('Enter the code', d['user_code'])
+
+        while True:
+            time.sleep(d['interval'])
+            status = authManager.CheckOAStatus(MY_ID)
+            print('status=', status)
+            if status == 'Success':
+                break
+
+        user = authManager.GetUserByID(MY_ID)
+
+    print('user=', user)
